4fbe6e0fad5620034a313ea9d334ee8512c45ddf
Tristan Huyck <thuyck3@gmail.com>
2020-10-26 23:26:22 -0400

    Functionalities include login/signup, post picture with caption, and view last 20 posts

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "VIfjHF3EzkNiQZjetXnIrUV18HXj4Zv7uCfgHYOn" // <- UPDATE
+                $0.clientKey = "CsxY7U59lfvcwQ4gDCydw7RnjjiqiDS3O12GybA9" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ parstagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  parstagram
+//
+//  Created by Tristan Alejandro Huyck on 10/26/20.
+//  Copyright © 2020 thuyck. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
